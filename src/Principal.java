ad301d0fb5b9e32891d6661460d78683fdfa767a
st_da <st_da@192.168.0.10>
2018-10-24 21:06:18 -0300

    error en el objeto

---
+++ src/Principal.java
@@ -0,0 +1,15 @@
+
+public class Principal {
+    
+    public static void main (String args[]){
+        
+        Saiayin s1 = new Saiayin();
+        
+        s1.nombre="Goku";
+        s1.nivel=4;
+        s1.planeta="Vegito";
+        s1.ki=152;
+        
+       
+    }
+}
---
+++ src/Saiayin.java
@@ -0,0 +1,17 @@
+
+public class Saiayin {
+    String nombre;
+    String planeta;
+    int nivel;
+    int ki;
+
+    public Saiayin(String nombre, String planeta, int nivel, int ki) {
+        this.nombre = nombre;
+        this.planeta = planeta;
+        this.nivel = nivel;
+        this.ki = ki;
+        System.out.println("***Datos del guerrero Z***");
+    }
+    
+   
+}
